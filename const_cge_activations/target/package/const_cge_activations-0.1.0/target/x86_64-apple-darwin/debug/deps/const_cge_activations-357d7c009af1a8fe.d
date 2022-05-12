e4ff720619d16f5625bcf1120111846241b7a2d8
Will Brickner <wgbrickner@gmail.com>
2022-05-12 18:07:49 -0500

    Fixed keywords in manifest

---
+++ const_cge_activations/target/package/const_cge_activations-0.1.0/target/x86_64-apple-darwin/debug/deps/const_cge_activations-357d7c009af1a8fe.d
@@ -0,0 +1,10 @@
+/Users/wbrickner/Documents/Projects/const_cge/const_cge_activations/target/package/const_cge_activations-0.1.0/target/x86_64-apple-darwin/debug/deps/const_cge_activations-357d7c009af1a8fe.rmeta: src/lib.rs src/activations/mod.rs src/activations/f32.rs src/activations/f64.rs
+
+/Users/wbrickner/Documents/Projects/const_cge/const_cge_activations/target/package/const_cge_activations-0.1.0/target/x86_64-apple-darwin/debug/deps/libconst_cge_activations-357d7c009af1a8fe.rlib: src/lib.rs src/activations/mod.rs src/activations/f32.rs src/activations/f64.rs
+
+/Users/wbrickner/Documents/Projects/const_cge/const_cge_activations/target/package/const_cge_activations-0.1.0/target/x86_64-apple-darwin/debug/deps/const_cge_activations-357d7c009af1a8fe.d: src/lib.rs src/activations/mod.rs src/activations/f32.rs src/activations/f64.rs
+
+src/lib.rs:
+src/activations/mod.rs:
+src/activations/f32.rs:
+src/activations/f64.rs:
