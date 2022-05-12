4d391be644f4465d6f8596a566befa4995de577b
Will Brickner <wgbrickner@gmail.com>
2022-05-12 18:08:49 -0500

    Removed recursive target in git tree

--- const_cge_activations/target/package/const_cge_activations-0.1.0/README.md
+++
@@ -1,6 +0,0 @@
-# Shared Activations Crate
-
-- This is a support crate for `const_cge`, allowing all generated code to reuse the same activation implementations.
-
-- If you want to use `const_cge`, you're in the wrong place.
-## Visit [`const_cge`](https://crates.io/crates/const_cge)
--- const_cge_activations/target/package/const_cge_activations-0.1.0/src/activations/f64.rs
+++
@@ -1,8 +0,0 @@
-pub const fn linear(x: f64) -> f64 { x }
-pub       fn threshold(x: f64) -> f64 { if x > 0.0 { 1.0 } else { 0.0 } }
-pub       fn relu(x: f64) -> f64 { if x > 0.0 { x } else { 0.0 } } 
-pub       fn sign(x: f64) -> f64 { if x > 0.0 { 1.0 } else if x == 0.0 { 0.0 } else { -1.0 } }
-pub       fn sigmoid(x: f64) -> f64 { 1.0 / (1.0 + (-x).exp()) }
-pub       fn tanh(x: f64) -> f64 { x.tanh() }
-pub       fn soft_sign(x: f64) -> f64 { x / (1.0 + x.abs()) }
-pub       fn bent_identity(x: f64) -> f64 { (((x.powi(2) + 1.0).sqrt() - 1.0) / 2.0) + x }
