320115062ac3a2c8dfbfe0a88ba3402e4c6eeb46
Will Brickner <wgbrickner@gmail.com>
2022-05-12 19:50:30 -0500

    Added automatic verbatim duplication of README.md into docs.rs module documentation, and code-hints hover in IDEs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-pub use const_cge_activations::*; // make `activations` available to codegen'd functions
-pub use const_cge_macro::*;       // make the macros available to users
+#![doc = include_str!("../README.md")] // mirror the README verbatim
+pub use const_cge_activations::*;      // make `activations` available to generated functions
+pub use const_cge_macro::*;            // make the macros available to users
 
-#[cfg(test)] mod tests;
+#[cfg(test)] mod tests;                // property test static evals against dynamic evals
